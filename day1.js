17f27f9f8daea06f4f588618a10c00eaa3ba636a
snehasreedharan05-tech <snehasreedharan.05@gmail.com>
2025-08-21 13:51:52 +0530

    Add files via upload
    Day 1 Assignment

---
+++ day1.js
@@ -0,0 +1,27 @@
+
+var city = "Bengaluru" // String
+console.log ('I live in' + " " + (city))
+console.log ((city)+ " " +  (typeof (city)))
+
+let marks = 85 // Number
+marks = 85+10
+console.log (marks)
+console.log ((marks)+ " " + (typeof (marks)))
+
+const isWeekend = true // Boolean
+console.log ("Relax")
+const isNotWeekend = false
+console.log ("Work")
+console.log (("Relax/Work") + " " + (typeof (isWeekend)))
+
+let futureGoal // Undefined
+console.log ((typeof (futureGoal)))
+
+
+// var allows redeclaration of varaible but is not safe to use as it may throw errors.
+// let does not allow redeclaration but allows reassign. It is block scoped.
+
+
+
+
+
